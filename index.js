6ebc3e3d21bac775db810fd97805594069b509d1
amogo solomon <amogoagaba@gmail.com>
2024-06-13 14:57:30 +0100

    add  api doc route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
+const fs = require('fs'); // Import the file system module
 
 const PORT = process.env.PORT || 3000;
 const options = {
@@ -43,6 +44,21 @@ app.get('/', (req, res) => {
     res.send('Welcome to Nkmor Healthcare APIs');
 });
 
+// Define a route for serving the api-docs.js file
+app.get('/api-docs.js', (req, res) => {
+    // Read the contents of api-docs.js file
+    fs.readFile('./swagger/api-docs.js', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading api-docs.js file:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        // Send the contents of the file as the response
+        res.type('application/javascript').send(data);
+    });
+});
+
+// Serve Swagger UI at /api-docs endpoint
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 app.use(express.json());
 app.listen(PORT, () => console.log(`The server is running on the PORT: ${PORT}`))
