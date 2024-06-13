aaf216d02fde874627c5345ecb2421097b0a631e
amogo solomon <amogoagaba@gmail.com>
2024-06-13 16:52:00 +0100

    update all files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
-const fs = require('fs'); // Import the file system module
 
 const PORT = process.env.PORT || 3000;
 const options = {
@@ -38,34 +37,6 @@ const options = {
 
 const app = express();
 const specs = swaggerJsDoc(options);
-
-// Define a route for the root URL
-/* app.get('/', (req, res) => {
-    res.send('Welcome to Nkmor Healthcare APIs');
-}); */
-
-app.get('/', (req, res) => {
-    res.send('Welcome to Nkmor Healthcare APIs');
-});
-
-
-// Define a route for serving the api-docs.js file
-app.get('./swagger/api-docs.js', (req, res) => {
-    // Read the contents of api-docs.js file
-    fs.readFile('./swagger/api-docs.js', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading api-docs.js file:', err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        // Send the contents of the file as the response
-        res.type('application/javascript').send(data);
-    });
-});
-
-// Serve Swagger UI at /api-docs endpoint
-app.use("./swagger/api-docs.js", swaggerUI.serve, swaggerUI.setup(specs))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 app.use(express.json());
 app.listen(PORT, () => console.log(`The server is running on the PORT: ${PORT}`))
-
-
